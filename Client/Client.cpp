3bcf11c751b71b66ff2b794d33c397a5176f902a
Dalina <zalupaevvyelfi@gmail.com>
2026-05-24 18:06:50 +0600

    finished the client part

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -1,11 +1,63 @@
 #include "client.hpp"
 #include <iostream>
+#include <vector>
 
 
 
-    Client::Client(boost::asio::io_context io): connectserver(io){}
+    Client::Client(boost::asio::io_context& io): connectserver(io)
+    {
+        check_path_file_save();
+    }
 
-    bool Client::connect(boost::asio::ip::address addressServet,unsigned short portserver)
+    void Client::check_path_file_save()
+    {
+
+       std::string filepath;
+
+       while(true)
+       {
+            std::cout << "Введите путь для сохранения фалов "<<std::endl;
+            std::getline(std::cin,filepath);
+            path_to_save_file = filepath;
+            if(!std::filesystem::exists(filepath))
+            {
+                    char exis;
+                    std::error_code err;
+                error("такого пути нет  хотите создать директорию по данному пути y/n");
+                    std::cin >> exis;
+                     std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+                    if(exis == 'y'|| exis == 'Y')
+                    {
+                        std::filesystem::create_directories(path_to_save_file,err);
+                        if(err)
+                        {
+                            error("ошибка"+err.message());
+                            continue;
+                        } 
+                        break;
+                    }
+                    else
+                    continue;
+                    
+            }
+            else
+            {
+                if(std::filesystem::is_directory(path_to_save_file))
+                {
+                    std::cout << "Все успкшно создано можете  начинать общение "<<std::endl;
+                    break;
+                }
+                    
+                else
+                {
+                    error("это не папка  укажите путь еще раз");
+                   continue;
+                }
+            }
+       }                     
+    }
+
+    void Client::connect(boost::asio::ip::address addressServet,unsigned short portserver)
     {
         connectserver.async_connect(boost::asio::ip::tcp::endpoint(addressServet,portserver),[this](const boost::system::error_code& er)
         {
@@ -23,7 +75,7 @@
     void Client::send_text(std::string& text)
     {
 
-        auto data = std::make_shared<DataSize>(Type::Text,0,text.size());
+        auto data = std::make_shared<DataSize>(Type::Text,0,text.size(),0);
         auto textHeap = std::make_shared<std::string>(std::move(text));
 
         boost::asio::async_write(connectserver,boost::asio::buffer(data.get(),sizeof(*data)),[this,data,textHeap](const boost::system::error_code& er,std::size_t size)
@@ -51,7 +103,7 @@
         std::filesystem::path path_file{path};
         if(!std::filesystem::exists(path_file) || !std::filesystem::is_regular_file(path_file)) 
         {
-            error("Фаил не найден укажите верный пкть");
+            error("Фаил не найден укажите верный путь");
             return;
         }     
          
@@ -79,12 +131,12 @@
                     return;
                 }
                 auto datafile = std::make_shared<DataSize>(Type::FileNext,0,0,data->idFile);
-                auto buffers = std::make_shared<char[]>(4096);
+                auto buffers = std::make_shared<std::vector<char>>(4096);
                 auto fuction = std::make_shared<std::function<void()>>();
 
                 *fuction = [this,self,datafile,fileread,buffers,fuction]()
                 {
-                    fileread->read(buffers.get(),4096);
+                    fileread->read(buffers->data(),4096);
                     std::streamsize realbyte{fileread->gcount()};
                     
                     if(realbyte > 0)
@@ -98,7 +150,7 @@
                                 return;
                             }
 
-                            boost::asio::async_write(connectserver,boost::asio::buffer(buffers.get(),datafile->data),[this ,self,fileread,buffers,fuction](const boost::system::error_code& code,std::size_t size)
+                            boost::asio::async_write(connectserver,boost::asio::buffer(buffers->data(),datafile->data),[this ,self,fileread,buffers,fuction](const boost::system::error_code& code,std::size_t size)
                             {
                                 if(code)
                                 {
@@ -125,14 +177,110 @@
 
     void Client::reverse_read()
     {
-        
+        auto self = shared_from_this();
+        auto data = std::make_shared<DataSize>();
+        boost::asio::async_read(connectserver,boost::asio::buffer(data.get(),sizeof(*data)),[this,self,data](const boost::system::error_code& er, std::size_t size)
+        {
+            if(er)
+            {
+                error("ошибка подключения");
+                return;
+            }
+            switch(data->type)
+            {
+                case (uint8_t)Type::Text:
+                    read_text(data->data);
+                break;
+
+                case (uint8_t)Type::FileNow:
+                    read_file_now(data->nameFile,data->data,data->idFile);
+                break;
+
+                case (uint8_t)Type::FileNext:
+                    read_file_next(data->data,data->idFile);
+                break;
+            }
+
+        });
 
     }
-    void Client::read_file(std::shared_ptr<DataSize> data)
+    void Client::read_file_now(uint32_t size_name,uint64_t data,uint32_t id)
     {
+        auto namefile = std::make_shared<std::string>();
+        namefile->resize(size_name);
+        auto self = shared_from_this();
+        boost::asio::async_read(connectserver,boost::asio::buffer(namefile.get(),size_name),[this,self,namefile,data,id](const boost::system::error_code& er, std::size_t size)
+        {
+            if(er)
+            {
+                reverse_read();
+                error("ошибка подключения");
+                return;
+            }
+            std::filesystem::path full_path_save_file = path_to_save_file / *namefile;
+            ActiveFileInfo info;
+            info.bytefile = data;
+            info.file = std::make_shared<std::ofstream>(full_path_save_file,std::ios::binary);
+
+            if(!info.file->is_open())
+            {
+                std::cout << "неудалось создать фаил на пути" + full_path_save_file.string() << std::endl;
+                reverse_read();
+                return;
+            }
+            Allfile[id] = info;
+        });
 
     }
-    void Client::read_text(std::shared_ptr<DataSize> data)
+    void Client::read_file_next(uint64_t data,uint32_t id)
     {
+        auto  self = shared_from_this();
+        auto it = Allfile.find(id);
+        if(it == Allfile.end() || !it->second.file->is_open())
+        {
+            reverse_read();
+            return;
+        }
+        auto buffers = std::make_shared<std::vector<char>>(data);
+        boost::asio::async_read(connectserver,boost::asio::buffer(buffers->data(),data),[this,id,data,self,buffers](const boost::system::error_code &er,std::size_t size)
+        {
+            if(er)
+            {
+                 error("соединение разорвано");
+                return;
+            }
+            auto& find_file = Allfile[id];
+            find_file.file->write(buffers->data(),size);
+            find_file.bytefile -= size;
+            if(find_file.bytefile == 0)
+            {
+                find_file.file->close();
+                Allfile.erase(id);
+
+                std::cout << "фаил успешно отправлен" << std::endl;
+            }
+            reverse_read();
+
+        });
+
+    }
+    void Client::read_text(uint64_t data)
+    {
+        auto buffertext = std::make_shared<std::vector<char>>(data);
+       
+        auto self = shared_from_this();
+
+        boost::asio::async_read(connectserver,boost::asio::buffer(buffertext->data(),data),[this,self,buffertext](const boost::system::error_code& er, std::size_t size)
+        {
+             if(er)
+            {
+                error("ошибка подключения");
+                return;
+            }
+
+            std::string text{buffertext->begin(),buffertext->end()};
+            std::cout << text <<std::endl;
+            reverse_read();
+        });
 
     }
--- Client/client.hpp
+++ Client/client.hpp
@@ -1,5 +1,5 @@
 #include "boost/asio.hpp"
-#include "EnetworkTypes/MessagType.hpp"
+#include "../EnetworkTypes/MessagType.hpp"
 #include <string>
 #include <memory>
 #include <filesystem>
@@ -9,21 +9,26 @@
 class Client : std::enable_shared_from_this<Client>
 {
     private:
-
+    
     boost::asio::ip::tcp::socket connectserver;
     uint32_t idsendfile = 0;
-    void error(std::string&& er);
+
     std::unordered_map<int32_t,ActiveFileInfo> Allfile;
+    std::filesystem::path path_to_save_file;
+
+    void check_path_file_save();
+    void error(std::string&& er);
 
     public:
 
-    Client(boost::asio::io_context io);
+    Client(boost::asio::io_context& io);
 
-    bool connect(boost::asio::ip::address addressServet,unsigned short portserver);
+    void connect(boost::asio::ip::address addressServet,unsigned short portserver);
     void send_text(std::string& text);
     void send_file(std::string&& path);
 
     void reverse_read();
-    void read_file(std::shared_ptr<DataSize> data);
-    void read_text(std::shared_ptr<DataSize> data);
+    void read_file_now(uint32_t size_name,uint64_t data,uint32_t id);
+    void read_file_next(uint64_t data,uint32_t id);
+    void read_text(uint64_t data);
 };
